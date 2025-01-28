10a3b1dad179125b95bc6a2905723e27bcfcdb86
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-01-28 20:56:56 +0000

    1

---
+++ prog52a.py
@@ -0,0 +1,7 @@
+length = int(input("Length = "))
+width = int(input("Width = "))
+Area = length * width
+perimeter = length * 2 + width * 2
+print("Area = ", Area)
+print("Perimeter = ", perimeter)
+# try_catch      input for scripts
