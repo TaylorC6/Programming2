98c86ee64c083dbfb8e324822b249435dcdd6d5f
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-03-10 15:24:59 -0500

    hi

---
+++ prog285b.py
@@ -0,0 +1,31 @@
+from cl285b import Salesperson
+
+
+def main():
+    try:
+        print("Number\tCode\tSale\tCommission")
+        with open("Langdat/prog285b.dat", 'r') as f:
+            for line in f:
+                data = line.split(" ")
+                id = int(data[0])
+                code = int(data[1])
+                sales = int(data[2])
+
+                person = Salesperson(id, code, sales)
+                person.calc_commission()
+                print(str(person))
+    except Exception as e:
+        print("Error:", e)
+
+    pass
+
+
+# Option 2: List Comprehension
+# id, code, sales = [float(x) for x in line.split(" ")]
+# Option 3: Conditional List Comprehension
+# id, code, sales = [float(x) if '.' in x else int(x) for x in line.split(" ")]
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ readwrite.py
@@ -0,0 +1,26 @@
+def main():
+    # Write Mode
+    with open("cool.txt", 'w') as f:
+        f.write('cool\n')
+        f.write('beans')
+
+    # Append Mode
+    with open("cool.txt", 'a') as file:
+        file.write('\nwowsers')
+
+    # Read Mode
+    with open("cool.txt", 'r') as f:
+        print(f.readlines())
+        for line in f:
+            print(line, end='')
+
+
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
