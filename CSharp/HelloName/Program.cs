68f1491e1e876b1b9347b0bd2512dbc865858f2c
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-04-09 15:09:51 -0500

    stuffC#

---
+++ CSharp/HelloName/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter your name: ");
+//int bool str char double(float) 
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name);
+
+Console.Write("Enter your age ");
+// int age = Convert.ToInt32(Console.ReadLine());
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("You are " + age + " years old you boomer...");
+Console.ReadKey(); // or ReadLine();
