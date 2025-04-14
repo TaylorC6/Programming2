84c1f176462e9241c0442395bf586e807d0ddbf8
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-04-14 15:28:01 -0500

    hi

---
+++ CSharp/prog54a/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace prog54a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.lblGal = new System.Windows.Forms.Label();
+            this.lblMpg = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1970 VW Bug",
+            "1979 Firebird",
+            "1980 Subaru",
+            "1975 Cutlass"});
+            this.comboBox1.Location = new System.Drawing.Point(152, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 41);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Car:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 52);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Miles:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 52);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Gallons::";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 52);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "MPG:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiles.Location = new System.Drawing.Point(152, 87);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(223, 52);
+            this.lblMiles.TabIndex = 5;
+            // 
+            // lblGal
+            // 
+            this.lblGal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGal.Location = new System.Drawing.Point(152, 174);
+            this.lblGal.Name = "lblGal";
+            this.lblGal.Size = new System.Drawing.Size(223, 52);
+            this.lblGal.TabIndex = 6;
+            // 
+            // lblMpg
+            // 
+            this.lblMpg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMpg.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMpg.Location = new System.Drawing.Point(152, 259);
+            this.lblMpg.Name = "lblMpg";
+            this.lblMpg.Size = new System.Drawing.Size(223, 52);
+            this.lblMpg.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(17, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 51);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(205, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 51);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(386, 391);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMpg);
+            this.Controls.Add(this.lblGal);
+            this.Controls.Add(this.lblMiles);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.Label lblGal;
+        private System.Windows.Forms.Label lblMpg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
--- prog701g.py
+++ prog701g.py
@@ -1,21 +1,55 @@
-from 701g import *
+from p701g import *
 
 
 def main():
     try:
         people = []
-        with open("data/prog701g.dat", 'r') as f:
+        with open("Langdat/prog701g.dat", 'r') as f:
             num = int(f.readline())
             while num != 99:
-                fn = f.readlines()
-                ln = f.readlines()
+                fn = f.readline()
+                ln = f.readline()
                 if num == 1:
                     gpa = float(f.readline())
                     p = Student(fn, ln, gpa)
                     people.append(p)
+                elif num == 2:
+                    numStu = int(f.readline())
+                    p = Teacher(fn, ln, numStu)
+                    people.append(p)
+                elif num == 3:
+                    favW = f.readline().strip()
+                    p = Admin(fn, ln, favW)
+                    people.append(p)
+                num = int(f.readline())
+
+            tot = 0.0
+            cnt = 0
+            totstus = 0
+            large = ""
+            sm = "ashaiuvfaihviaiuvwuvhilvvdiubwieiufhgiuszhdbKVCJBXZLIVHGifuhgaoiewsgifsghoilblkjdxzvhblijzdshgfpiuewahgifauhdsikjbz;kjvdvha fphwa uehgkjf,mz.njvsahf[hfkjnds;vnjhsag;jhkjdsba;fkjb kjdxzvb,mcxzbv;shagtaewhoifhp;dskjabv,mbcxzkjvhshe;af"
+
+            for person in people:
+                if isinstance(person, Student):
+                    tot += person.gpa
+                    cnt += 1
+                if isinstance(person, Teacher):
+                    totstus += person.numStudents
+                if isinstance(person, Admin):
+                    favW = person.favWord
+                    if len(favW) > len(large):
+                        large = favW
+                    if len(favW) < len(sm):
+                        sm = favW
+
+            print("Average student GPA:", round(tot/cnt, 2))
+            print("Total number of students taught:", totstus)
+            print("Smallest favorite admin word:", sm)
+            print("Largest favorite admin word:", large)
 
     except Exception as e:
         print("Error:", e)
+        raise e
 
 
 if __name__ == "__main__":
