1fbcf4d71c4b0b0385e367ac138ce5666ef92447
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-05-05 15:26:55 -0500

    fdsa

---
+++ CSharp/CalcWithFunctions/Program.cs
@@ -0,0 +1,28 @@
+﻿// <access level> <static or not> <return type> name(<args>) {}
+// In console apps, we'll make all functions "static"
+// Not static in Forms appsL always "public" though
+
+static int add(int x, int y) { return x + y; }
+static int sub(int x, int y) { return x - y; }
+static int mul(int x, int y) { return x * y; }
+static int div(int x, int y) { return x / y; }
+
+static void wait() { // void means "returns nothing"
+    Console.ReadLine();
+    // return;
+}
+
+Random rand = new Random();
+int n1 = rand.Next(1,101);
+int n2 = rand.Next(150, 201);
+Console.Write("Choose an option: add, sub, mul, or div: ");
+string op = Console.ReadLine().ToLower();
+int result = 0;
+if (op == "add") result = add(n1, n2);
+else if (op == "sub") result = sub(n1, n2);
+else if (op == "mul") result = mul(n1, n2);
+else if (op == "div") result = div(n1, n2);
+else Console.WriteLine("ERROR");
+Console.WriteLine("num1=" + n1 + "\t num2=" + n2);
+Console.WriteLine("Result: " + result);
+wait();
---
+++ CSharp/pg435TicketSales/StudentForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace pg435TicketSales {
+    partial class StudentForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(121, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(435, 404);
+            this.Controls.Add(this.button1);
+            this.Name = "StudentForm";
+            this.Text = "StudentForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StudentForm_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
