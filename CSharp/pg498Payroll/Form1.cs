598a1fc88f43ac6b13f3b9e423239d663ca88404
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-05-08 15:25:56 -0500

    sffds

---
+++ CSharp/pg498Payroll/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pg498Payroll {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            const int intMAX_EMPLOYEES = 5;
+            const decimal decHOURLY_PAY_RATE = 6.0m;
+            // Calc and display gross Pay Earned by Employees
+            int[] Hours = new int[intMAX_EMPLOYEES];  // An array   [0,0,0,0,0]
+            // <type>[] varName = new <type>[capacity];
+            int Count = 0;
+            int EmpHours = 0;
+            decimal EmpPay = 0.0m;
+
+            for (Count = 0; Count < intMAX_EMPLOYEES; Count++) {
+                while (int.TryParse(Interaction.InputBox("Enter # of Hours Worked by Employee #" + (Count + 1).ToString(), "Need Hours Worked"), out EmpHours) == false) {
+                    MessageBox.Show("Please Enter an Integer for Hours Worked");
+                }
+                Hours[Count] = EmpHours;
+            }
+            listBox1.Items.Clear();
+            for (Count = 0; Count < intMAX_EMPLOYEES; Count++) {
+                EmpPay = Hours[Count] * decHOURLY_PAY_RATE;
+                listBox1.Items.Add("Employee " + (Count + 1).ToString() +
+                " Earned " + EmpPay.ToString("$.00"));
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
--- CSharp/prog535CatchMe/Form1.cs
+++ CSharp/prog535CatchMe/Form1.cs
@@ -10,18 +10,18 @@ using System.Windows.Forms;
 
 namespace prog535CatchMe {
     public partial class Form1 : Form {
+
+        string[] strCaption = { "Click Here", "Try Harder", "Too Slow", "Try Again",
+            "Not Even Close", "I'm over here"};
+
+        Random rand = new Random();
+
         public Form1() {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e) {
 
-            string[] strCaption = { "Click Here", "Try Harder", "Too Slow", "Try Again", 
-            "Not Even Close", "I'm over here"};
-
-            Random rand = new Random();
-        }
-        private void btn_catch_click(object sender, EventArgs e) {
             MessageBox.Show("You Got Me!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             Application.Exit();
         }
