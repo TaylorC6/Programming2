a57895e3ad695596308ad593ca57c10ebe05af8d
TaylorC6 <cahill.t1@stu.janesville.k12.wi.us>
2025-05-15 14:39:41 -0500

    edsg

---
+++ CSharp/listDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿//List<type> var = new List<type>() { x, y, z };
+// List<string> text = [];
+// List<string> lines = new List<string>();
+
+// List<int> nums = [1, 2, 3];
+
+List<int> nums = new List<int>() { 1, 2, 3 };
+
+nums.Add(4);
+nums.Add(5);
+nums.Add(6);
+nums.Add(7);
+nums.RemoveAt(5); // Removes 6
+nums.Remove(7); // Removes 7
+Console.WriteLine("Length: " + nums.Count);
+Console.WriteLine(string.Join(", ", nums)); // "1, 2, 3, 4, 5
+
+Console.WriteLine("foreach:");
+foreach (int n in nums)
+    Console.WriteLine(n);
+
+Console.ReadLine();
+
+// Look at C# Documentation on MSDN for all List methods
---
+++ CSharp/pg273MassAndWeight/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter the mass of an object: ");
+int mass = int.Parse(Console.ReadLine());
+double weight = 0.0;
+string msg = "";
+if (mass > 10 && mass < 1000) {
+    weight = mass * 9.8;
+    msg = "The weight is " + weight;
+}
+else if (mass > 1000) {
+    msg = "Too heavy!";
+}
+else if (mass < 10) {
+    msg = "Too light!";
+}
+Console.WriteLine(msg);
+Console.ReadLine();
